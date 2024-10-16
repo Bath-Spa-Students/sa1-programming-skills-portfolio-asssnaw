e032a99166e44c5a5f73db836173ba341a5342af
asssnaw <asnafatima2006@gmail.com>
2024-10-16 14:48:22 +0400

    Update Coding is cool.py
    coding is cool
    -Asna Fatima

--- Exercise 01/Coding is cool.py
+++ Exercise 01/Coding is cool.py
@@ -1,11 +1,12 @@
-#Assigning the first word to variable word1 
+#Assigning the first word to word1 
 word1 = "coding"
 
-#Assigning the second word to variable word2
+#Assigning the second word to word2
 word2 = "is"
 
-#Assigning the third word to the variable word3
+#Assigning the third word to the word3
 word3 = "cool"
 
+#Adding up all the word variables to get the desired output and then print the varriable 
 c = word1 + " " + word2 + " " + word3
 print (c)
