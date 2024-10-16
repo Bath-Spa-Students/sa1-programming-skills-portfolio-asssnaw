1c966ccdd2d20e1de60c55d9782e1823c03eb0c7
asssnaw <asnafatima2006@gmail.com>
2024-10-16 14:43:39 +0400

    Create Coding is cool.py
    coding is cool
    - Asna Fatima

---
+++ Exercise 01/Coding is cool.py
@@ -0,0 +1,11 @@
+#Assigning the first word to variable word1 
+word1 = "coding"
+
+#Assigning the second word to variable word2
+word2 = "is"
+
+#Assigning the third word to the variable word3
+word3 = "cool"
+
+c = word1 + " " + word2 + " " + word3
+print (c)
