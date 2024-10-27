d670f2d7831c1d0000e753948e3aec4aac7c3d3f
asssnaw <asnafatima2006@gmail.com>
2024-10-27 20:44:48 +0400

    Simplesum.py
    Simple sum question
    - Asna Fatima

---
+++ Exercise 02/Simplesum.py
@@ -0,0 +1,11 @@
+# We declare the first variable NO1 with the value '8'
+NO1 = 8
+
+# We declare the second variable NO2 with the value '10'
+NO2 = 10
+
+# Now we add both the variable interger values
+Sum = (NO1 + NO2)
+
+# In the final step we print the sum to get the output
+print(Sum)
