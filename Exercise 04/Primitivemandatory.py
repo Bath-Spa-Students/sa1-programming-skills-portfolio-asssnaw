8fe0e8c6fc4721a4978b2875e66c23047a48f313
asssnaw <asnafatima2006@gmail.com>
2024-10-27 20:56:44 +0400

    Primitivemandatory.py
    Primitive Quiz mandatory question
    - Asna Fatima

---
+++ Exercise 04/Primitivemandatory.py
@@ -0,0 +1,20 @@
+# The following is the code for the Primitive Mandatory Question
+# Firstly we will write a code that asks the user "What is the capital of France" and waits for their response.
+
+# The question will be assigned to the variable "Question"
+
+Question = str(input("What is the capital of France?"))
+
+# If the user"s answer is correct (i.e. Paris )
+
+if Question == "Paris":
+    print("The answer is correct.")
+
+# Incase of correct answer the statement "The answer is correct" will appear on the screen
+
+# If the user types the wrong answer
+
+else :
+    print("The answer is wrong.")
+
+# Incase of wrong answer the ststement "The answer is wrong" will appear on the screen``
