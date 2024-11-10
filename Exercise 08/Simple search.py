ed2f9f41ad1e0969c59489ec5b3ef91e542d7efe
asssnaw <asnafatima2006@gmail.com>
2024-11-10 17:25:40 +0400

    Simple search.py
    this is the mandatory question code of exercise 08

--- Exercise 08/Simple search.py
+++ Exercise 08/Simple search.py
@@ -1,10 +1,13 @@
 # These are the list of names
 names = ["Jake", "Zac" , "Ian", "Ron" , "Sam" , "Dave"]
 
+# this code is used to promt the user for a name that is the user wants to search
 search = 'Sam'
 
+#If the name entered by the user is mentioned in the list the following statement will be printed
 if search in names:
     print(f"{search} found in the list!")
+#If the name entered by the user is not mentioned in the list the following statement will be printed
 else:
     print(f"{search} not found in the list.")
     
