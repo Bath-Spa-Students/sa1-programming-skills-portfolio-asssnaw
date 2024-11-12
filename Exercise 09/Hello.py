7770ff0fee3f6a950d1f9b9435b3246ab655c9a1
asssnaw <asnafatima2006@gmail.com>
2024-11-12 20:57:10 +0400

    Hello.py
    this is fill in the blank

---
+++ Exercise 09/Hello.py
@@ -0,0 +1,11 @@
+
+def hello():
+    print("Hello")  # This code prints "Hello" to  console.
+
+def main():
+    hello()  # This code calls the hello() function to get exicuted.
+
+# The bellow text checks if this script is being run directly or not.
+if __name__ == "__main__":
+    main()  
+ # The above code then runs main() function if the script is run directly
